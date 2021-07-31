9d0ec80ba37cc80e3c08d688bc8649f336fa9446
Moisés Vilas Boas <moisesboas@gmail.com>
2021-07-31 15:51:45 -0300

    feat: consulta de vários usuários

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import { ApolloServer, gql } from 'apollo-server';
 
 const server = new ApolloServer({
   typeDefs: gql`
+    # Query é o type Padrão inicializador
     type Query {
       hello: String
       hi: String
       user: User
+      users: [User!]!
     }
 
+    #User e um Custom Type || Object Type
     type User {
       id: ID
       nome: String
@@ -32,6 +35,42 @@ const server = new ApolloServer({
           decimais: 42.42,
           humano: true,
         };
+      },
+      users: () => {
+        return [
+          {
+            id: '23F2332DSfd',
+            nome: 'Moisés',
+            sobrenome: 'Vilas Boas',
+            idade: 23,
+            decimais: 42.42,
+            humano: true,
+          },
+          {
+            id: 'f23t32gsd',
+            nome: 'Anabelle',
+            sobrenome: 'Segatto',
+            idade: 23,
+            decimais: 42.42,
+            humano: true,
+          },
+          {
+            id: '12d12d2112d12',
+            nome: 'Stifler',
+            sobrenome: 'dos Santos',
+            idade: 12,
+            decimais: 42.42,
+            humano: true,
+          },
+          {
+            id: 'f23324',
+            nome: 'Moisés',
+            sobrenome: 'Vilas Boas',
+            idade: 23,
+            decimais: 42.42,
+            humano: true,
+          },
+        ];
       },
     },
   },
